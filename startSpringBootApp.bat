192bf8bf3872750c8244c6ad49396c95f95c628b
yichongyang <33671891+yichongyang@users.noreply.github.com>
2021-12-29 10:34:41 -0500

    Add -d to detach the running container

--- startSpringBootApp.bat
+++ startSpringBootApp.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start /B docker run -p 443:443 --name yyang-spring-boot-app yyang/spring-boot-app > nul 2>&1
+start /B docker run -d -p 443:443 --name yyang-spring-boot-app yyang/spring-boot-app > nul 2>&1
 
 exit 0
